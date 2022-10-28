1906e7759e58e2771debdb7308c275b14d305185
DeepjyotiDeb <55659244+DeepjyotiDeb@users.noreply.github.com>
2022-10-28 19:03:49 +0530

    first commit

---
+++ src/server/router.ts
@@ -0,0 +1,40 @@
+import { initTRPC, TRPCError } from "@trpc/server";
+import { hash } from "argon2";
+
+import { IContext } from "./context";
+import { signUpSchema } from "../common/validation/auth";
+import { randomUUID } from 'crypto';
+
+const t = initTRPC.context<IContext>().create();
+
+export const serverRouter = t.router({
+  signup: t.procedure.input(signUpSchema).mutation(async ({ input, ctx }) => {
+    const { username, email, password } = input;
+
+    const exists = await ctx.prisma.user.findFirst({
+      where: { email },
+    });
+
+    if (exists) {
+      throw new TRPCError({
+        code: "CONFLICT",
+        message: "User already exists.",
+      });
+    }
+
+    const salt = randomUUID()
+    const hashedPassword = await hash(password + salt);
+
+    const result = await ctx.prisma.user.create({
+      data: { username, email, password: hashedPassword, salt },
+    });
+
+    return {
+      status: 201,
+      message: "Account created successfully",
+      result: result.email,
+    };
+  }),
+});
+
+export type IServerRouter = typeof serverRouter;
